fba2c2346ea6fd0d836051caf91819dfe3043255
ryoasaka12345 <ryo.asaka@g.pascalia.co.jp>
2021-06-24 14:34:08 +0900

    Dynamic routing with params 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Route, useParams } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
@@ -11,15 +11,17 @@ function App() {
     <div>
       <MainHeader />
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </    Route>
-        <Route path='/product-detail/:productId'>
-          <ProductDetail />
-        </Route>
+        <Switch>
+          <Route path="/welcome" exact>
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </    Route>
+          <Route path='/product-detail/:productId' exact>
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
