597552f2eba6ac9d4c843470d0514c4332c13a86
ryoasaka12345 <ryo.asaka@g.pascalia.co.jp>
2021-06-24 11:54:08 +0900

    Add link to Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 
-import { Route} from "react-router-dom";
+import { Route, Link} from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 
 function App() {
   return (
     <div>
+      <header>
+        <ul>
+          <li><Link to="/welcome">Welcome</Link></li>
+          <li><Link to="/products">Products</Link></li>
+        </ul>
+      </header>
       <Route path="/Welcome">
         <Welcome />
       </Route>
