e24f4858621a3b763fdd31a3aab0c27e683bf842
ryoasaka12345 <ryo.asaka@g.pascalia.co.jp>
2021-06-24 11:44:32 +0900

    How to use React Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { Route} from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route path="/Welcome">
+        <Welcome />
+      </Route>
+      <Route path="/Products">
+        <Products />
+      </Route>
     </div>
   );
 }
---
+++ src/pages/Products.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Products() {
+    return <h2>Products to our website</h2>;
+}
+
+export default Products;
