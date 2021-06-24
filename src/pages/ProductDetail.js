ec6daab78fa53afe584c2930a883a4153420d681
ryoasaka12345 <ryo.asaka@g.pascalia.co.jp>
2021-06-24 15:29:25 +0900

    Load detail

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 
-import { useParams } from 'react-router-dom';
+import { useParams, Route } from 'react-router-dom';
 
 function ProductDetail() {
     const params = useParams();
-    return <li>Product Detail, ID={params.productId}</li>
+    const [product, setProduct] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const productDetailAPI = `https://60caadb921337e0017e42cf9.mockapi.io/products/${params.productId}/`;
+    const fetchProductDetailHandler = () => {
+        setIsLoading(true);
+        fetch(productDetailAPI)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+                setProduct(data);
+                setIsLoading(false);
+            });
+    }
+
+    useEffect(() => {
+        fetchProductDetailHandler();
+    }, []);
+
+    const ProductDetail = () => {
+        return (
+            <Fragment>
+                <h2>{product.name}</h2>
+                <p>{product.description}</p>
+            </Fragment>
+        );
+    }
+
+    return (
+        <Fragment>
+            {isLoading && <p>Loading Product Data...</p>}
+            {!isLoading && <ProductDetail />}
+        </Fragment>
+    );
 }
 
 export default ProductDetail;
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 function Products() {
+    const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const productsListAPI = 'https://60caadb921337e0017e42cf9.mockapi.io/products';
+    const fetchProductsHandler = () => {
+        setIsLoading(true);
+        fetch(productsListAPI)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+                setProducts(data);
+                setIsLoading(false);
+            });
+    }
+
+    useEffect(() => {
+        fetchProductsHandler();
+    }, []);
+
     return (
         <div>
-            <h2>Welcome to products page</h2>
+            <h2>Products List</h2>
+            {isLoading && <p>Loading products list...</p>}
             <ul>
-                <li>
-                    <Link to="/product-detail/1">Product 1</Link>
-                </li>
-                <li>
-                    <Link to="/product-detail/2">Product 2</Link>
-                </li>
-                <li>
-                    <Link to="/product-detail/3">Product 3</Link>
-                </li>
+                {products.map((product) => (
+                    <li key={product.id}>
+                        <Link to={`/product-detail/${product.id}`}>{product.name}</Link>
+                    </li>
+                ))}
             </ul>
         </div>
-    );
+    )
 }
 
 export default Products;
